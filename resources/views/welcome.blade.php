7eca330eafa736e22651c2c2675b900205223bd6
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-04-28 23:46:19 +0100

    feat(ui): fetch waste api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,25 @@
 <!doctype html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Laravel Project</title>
 
+    <script>
+        let x = async () => {
+            await fetch('/waste')
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }
+    </script>
+
     <!-- Stylesheet -->
     @vite('resources/css/app.css')
 </head>
 
 <body>
-
+<div class="bg-red-400" onclick="x()">Test</div>
 </body>
 
 </html>
